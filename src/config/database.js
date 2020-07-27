ebc903c52f2d587deafc68180da121e43b54f2f0
Armando Jaleo <armando@armandojaleo.com>
2020-07-28 01:24:23 +0200

    Public files github

--- src/config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  DB: 'mongodb+srv://ad-directory:2A4zcaauwRQVYFEU@ad-directory.07okk.mongodb.net/ad-directory?retryWrites=true&w=majority'
-};
--- src/config/secret.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret: 'kjgojwrtoijgiogw48uy98w5i9g9wjgj5984jgwjg89tr'
-}
